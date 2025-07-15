6b26721eab49960627a938955cdd7f30d37600a3
HafiNight <50204304+HafiNight@users.noreply.github.com>
2025-07-15 10:13:04 +0200

    Add files via upload

---
+++ pysr/trainmodel7.py
@@ -0,0 +1,62 @@
+#Import
+import pandas as pd
+import numpy as np
+
+#Data
+import pickle
+
+#Model
+from pysr import PySRRegressor, TensorBoardLoggerSpec
+from sklearn.model_selection import train_test_split
+
+# Import data
+with open('../data/df_filtered_rearranged.pkl', 'rb') as f:
+    df = pickle.load(f)
+
+df['mix'] = df['compA'] + df['compB']
+mixtures = df['mix'].unique()
+
+train_mix, test_mix = train_test_split(mixtures, test_size=0.3, random_state=42)
+
+df = df[df['P'] != 77000]
+X_columns = ['mA', 'mB', 'KA', 'KB']
+X = df[X_columns]
+y = df['K']
+
+train_df = df[df['mix'].isin(train_mix)]
+test_df = df[df['mix'].isin(test_mix)]
+
+X_train = train_df[X_columns]
+y_train = train_df['K']
+X_test = test_df[X_columns]
+y_test = test_df['K']
+
+# Create a logger that writes to "logs/run*":
+logger_spec = TensorBoardLoggerSpec(
+    log_dir="logs/run",
+    log_interval=10,  # Log every 10 iterations
+)
+
+model = PySRRegressor(
+    logger_spec=logger_spec,
+    niterations= 100000,
+    populations = 48,
+    maxsize = 40,
+    unary_operators=["sqrt", "abs", "log"],
+    parsimony=0.0000001,
+    warmup_maxsize_by = 0.85,
+    turbo=True,
+    nested_constraints = {"sqrt":{"sqrt":0},"log":{"log":0}},
+    dimensional_constraint_penalty = 10**5,
+    early_stop_condition=(
+        "stop_if(loss, complexity) = loss < 1e-6 && complexity < 20"
+        # Stop early if we find a good and simple equation
+                          ),
+)
+
+model.fit(
+    X_train, 
+    y_train,
+    X_units=["1", "1", "W*m^-1*K^-1", "W*m^-1*K^-1"],
+    y_units="W*m^-1*K^-1"
+    )
